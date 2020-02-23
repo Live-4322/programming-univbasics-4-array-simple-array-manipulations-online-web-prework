293caf92fb396c73c9c0f2aa25a32f0784cea281
Charlie Oster <oster.charlie@yahoo.com>
2020-02-23 15:48:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+colors_in_the_rainbow ["red","orange","yellow","green","blue","indigo"]
+colors_in_the_rainbow.push ("violet")
+
+bouroughs_in_nyc ["Brooklyn","Queens","Manhattan","Bronx"]
+@neighborhood = "Staten Island"
+bouroughs_in_nyc.unshift (@neighborhood)
+
+@continents ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+@continents.pop
+
+@dog_breeds ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+
+@my_favorite_cities ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+@im_so_over_this_citys.shift 
+
+@icecream_brands ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
+@my_favorite_things ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
+
+@list_of_programing_languages ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language ["Python"]
+
+
